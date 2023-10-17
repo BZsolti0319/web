e54d1bdd793865302e40b4c8100af0ef1894e4ff
BZsolti0319 <83708317+BZsolti0319@users.noreply.github.com>
2023-10-17 09:30:48 +0200

    Add files via upload

---
+++ javito.js
@@ -0,0 +1,5 @@
+let containerszin=document.getElementById(container);
+let elsodoboz=document.getElementsByClassName(doboz1);
+let masodikdoboz=document.getElementsByClassName(doboz2);
+let harmadikdoboz=document.getElementsByClassName(doboz3);
+let weboldalszin=document.body();
